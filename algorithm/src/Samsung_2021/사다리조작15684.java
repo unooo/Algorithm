c2f046ee55e7b974f513742941f4e568623df822
Unoo <bunge24@gmail.com>
2021-03-19 21:56:20 +0900

    알고리즘 풀이

--- algorithm/src/Samsung_2021/사다리조작15684.java
+++ algorithm/src/Samsung_2021/사다리조작15684.java
@@ -1,5 +1,17 @@
 package Samsung_2021;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class 사다리조작15684 {
 
+	static int N, M ,H;
+	static int map[][];
+	
+	public static void main(String[] args){
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+	}
+	
+	
 }
