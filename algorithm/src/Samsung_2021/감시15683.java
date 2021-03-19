3743784ce6c6ad17b507d8500c4d66dad4da2d64
Unoo <bunge24@gmail.com>
2021-03-19 21:59:59 +0900

    알고리즘 풀이

--- algorithm/src/Samsung_2021/감시15683.java
+++ algorithm/src/Samsung_2021/감시15683.java
@@ -15,7 +15,7 @@ public class °¨½Ã15683 {
 	static int dy[] = { -1, 0, 1, 0 };
 	static int dx[] = { 0, 1, 0, -1 };
 	static LinkedList<Node> camList;
- 
+  
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
